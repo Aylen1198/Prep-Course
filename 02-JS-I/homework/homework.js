f20cf1c731f5efe1480799d995a12f7d049a7a5b
AYLEN <aylengenova1@gmail.com>
2022-06-13 01:47:45 -0300

    prueba git

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -214,9 +214,9 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u' ){
-    return 'Es vocal'
+    return 'Es vocal';
   } else {
-    return 'Dato incorrecto'
+    return 'Dato incorrecto';
   } 
 }
 
