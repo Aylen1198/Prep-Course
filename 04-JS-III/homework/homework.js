7b1332c48317959d1513dde2379f7ef43d18c726
AYLEN <aylengenova1@gmail.com>
2022-06-14 04:03:09 -0300

    prueba arrayContiene

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -71,6 +71,11 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for (let i=0; i < array.length; i++){
+    if (array[i] == elemento)
+    return true;
+  }
+  return false;
 }
 
 
